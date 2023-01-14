aebf428f22af4220c80fb6e87692bb540edb40e0
Chris-ASUS <humbertpt14@gmail.com>
2023-01-14 10:21:47 -0500

    Retornando imagen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,64 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getMovies();
+  }
+
+  getMovies() async {
+    Uri _uri = Uri.parse(
+      "https://api.themoviedb.org/3/discover/movie?api_key=b023410500aafb2c79fe3179a1da5f64",
+    );
+    http.Response response = await http.get(_uri);
+    if (response.statusCode == 200) {
+      // print(response.body);
+      Map<String, dynamic> myMap = json.decode(response.body);
+      movies = myMap["results"];
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                itemCount: movies.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: double.infinity,
+                    height: height * 0.7,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: NetworkImage(
+                        "https://image.tmdb.org/t/p/w500${movies[index]["poster_path"]}",
+                      ),
+                    )),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
